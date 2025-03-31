686bca4367008544e9fe75a026ee3448c5b37c73
Young SHAW <young0723584@gmail.com>
2025-03-31 22:13:27 +0800

    feat: finish lab02

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -7,9 +7,9 @@ unsigned get_bit(unsigned x,
                  unsigned n) {
     // YOUR CODE HERE
     // Returning -1 is a placeholder (it makes
-    // no sense, because get_bit only returns 
+    // no sense, because get_bit only returns
     // 0 or 1)
-    return -1;
+    return (x >> n) & 0x1;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
@@ -17,11 +17,17 @@ void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
+    unsigned origin = *x;
+    unsigned changed = get_bit(*x, n) ^ v;
+    changed = changed << n;
+    *x = origin ^ changed;
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
     // YOUR CODE HERE
+    unsigned bit = get_bit(*x, n);
+    set_bit(x, n, (!bit) & 0x01);
 }
 
--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,8 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t origin = *reg;
+    uint16_t msb = ((origin ^ (origin >> 2)) ^ (origin >> 3) ^ (origin >> 5)) & 0x01;
+    *reg = (origin >> 1) | (msb << 15);
 }
 
